ef63df04a79f87b6b858489adcae8d03d44535bc
HARRIS-IRFAN-123 <113856187+HARRIS-IRFAN-123@users.noreply.github.com>
2022-09-25 11:24:30 -0700

    Add files via upload

---
+++ MY-SECOND-PROGRAM.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main (){
+	short testVar = 32737;
+	printf("%d\n",testVar);
+	testVar = testVar + 1;
+	printf("%d\n", testVar);
+	testVar = testVar - 1;
+	printf("%d\n", testVar);
+	getch();
+}
