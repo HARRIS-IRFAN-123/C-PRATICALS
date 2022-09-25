6eb924c382e76eeb58a6c69f6490315eb78678b7
HARRIS-IRFAN-123 <113856187+HARRIS-IRFAN-123@users.noreply.github.com>
2022-09-25 11:41:29 -0700

    Add files via upload

---
+++ MY-THIRD-NUMBER.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int a,b;
+	a = 10;
+	b = 5;
+	printf("a + b = %d\n", a + b);
+	printf("a - b = %d\n", a -b);
+	printf("a * b = %d\n", a * b);
+	printf("a / b = %d\n", a/b);
+	printf("a%%b = %d\n",a % b);
+	getch();
+}
