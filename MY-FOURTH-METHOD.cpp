29128123f4ea21cec5d036ea62f0c62426791b56
HARRIS-IRFAN-123 <113856187+HARRIS-IRFAN-123@users.noreply.github.com>
2022-09-25 11:59:06 -0700

    Add files via upload

---
+++ MY-FOURTH-METHOD.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main (){
+	int a;
+	a = 10;
+	printf("Value of a : %d\n", a);
+	a += 5;
+	printf("Value of a after a+=5: %d\n", a);
+	a-=5;
+	printf("Value of a after a-=5: %d\n", a);
+	a*=2;
+	printf("Value of a after a *=2: %d\n",a);
+	a/=2;
+	printf("Value of a after a/=2: %d\n", a);
+	a%=2;
+}
