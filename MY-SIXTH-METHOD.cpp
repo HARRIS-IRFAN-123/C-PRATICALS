a1985885817b18cba101fe4f6bfe22772322554d
HARRIS-IRFAN-123 <113856187+HARRIS-IRFAN-123@users.noreply.github.com>
2022-09-26 11:33:30 -0700

    Add files via upload

---
+++ MY-SIXTH-METHOD.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+	printf("HELLO WORLD %d\n");
+	printf("I AM LEARNING THE C PROGRAMMING LANGUAGE %d\n");
+	printf("IT IS VERY INTEREESTING PROGRAMMING LANGUAGE");
+		
+}  
