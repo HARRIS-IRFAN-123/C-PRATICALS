813eb3eaa2619232b99ce29a1befc3114c1c1ac3
HARRIS-IRFAN-123 <113856187+HARRIS-IRFAN-123@users.noreply.github.com>
2022-09-29 12:46:10 -0700

    Add files via upload

---
+++ MY-TENTH-PROGRAM.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+	int myNum = 50;
+	float myFloatNum = 5.99;
+	char myLetter = 'D';
+	printf("%d\n", myNum);
+	printf("%f\n", myFloatNum);
+	printf("%c\n", myLetter);
+}
+	
+
